eacb828cf5f8d887fdcdafa377fd95dd481cc1d1
harinirao1998 <harinirao1998@gmail.com>
2022-02-27 23:31:14 -0500

    adding files

---
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.flixster
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class MovieAdapter(private val context: Context, private val movies: List<Movie>)
+    : RecyclerView.Adapter<MovieAdapter.viewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_movie,parent, false)
+        return viewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+       val movie=movies[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount() = movies.size
+    inner class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        private val ivPoster=itemView.findViewById<ImageView>(R.id.ivPoster)
+        private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
+        private val tvOverView = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        fun bind(movie:Movie){
+            tvTitle.text=movie.title
+            tvOverView.text=movie.overview
+            //populate imageView
+            Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+        }
+    }
+
+}
